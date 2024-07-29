756dfc24aded39244aafc254a3fc4a8256294c45
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-07-29 09:32:45 +0200

    Implement basic functions (add, subtract, multiply, divide).

---
+++ main.py
@@ -0,0 +1,35 @@
+# Add
+def add(n1, n2):
+    return n1 + n2
+
+
+# Subtract
+def subtract(n1, n2):
+    return n1 - n2
+
+
+# Multiply
+def multiply(n1, n2):
+    return n1 * n2
+
+
+# Divide
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+num1 = int(input("Input the first number: "))
+for symbol in operations:
+    print(symbol)
+operation_symbol = input("Pick an operation from the line above: ")
+num2 = int(input("Input the second number: "))
+
+result = operations[operation_symbol](num1, num2)
+print(f"{num1} {operation_symbol} {num2} = {result}")
