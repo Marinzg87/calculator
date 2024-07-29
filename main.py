24f71760d8d79ef44a62475735e7fe0897d587f8
Marcin Bogusz <bogusz.marcin@icloud.com>
2024-07-29 10:03:17 +0200

    Implement continue calculations with previous result and start the new calculation.

--- main.py
+++ main.py
@@ -25,11 +25,26 @@ operations = {
     "/": divide
 }
 
-num1 = int(input("Input the first number: "))
-for symbol in operations:
-    print(symbol)
-operation_symbol = input("Pick an operation from the line above: ")
-num2 = int(input("Input the second number: "))
 
-result = operations[operation_symbol](num1, num2)
-print(f"{num1} {operation_symbol} {num2} = {result}")
+def calculator():
+    num1 = int(input("Input the first number: "))
+    for symbol in operations:
+        print(symbol)
+
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("Input the next number: "))
+
+        result = operations[operation_symbol](num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {result}")
+
+        if input(f"Type 'y' to continue calculating with {result},"
+                 f" or type 'n' to start a new calculation: ") == 'y':
+            num1 = result
+        else:
+            should_continue = False
+            calculator()
+
+calculator()
